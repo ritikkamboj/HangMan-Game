e1aa1bce54cc12df90bafacd48f5e8f5cf0cc958
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-08-16 19:50:25 +0530

    facing issue in js logic to get the correct letter from the selected letters

---
+++ script.js
@@ -0,0 +1,30 @@
+const word = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const playAgain = document.getElementById("play-again");
+const finalMessage = document.getElementById("final-message");
+
+const words = ["application", "programming", "interface", "wizard"];
+
+const selecteWord = words[Math.floor(Math.random() * words.length)];
+console.log( selecteWord); //string
+const x = selecteWord.split(" ");
+console.log(x);
+console.log(typeof x);
+
+console.log(selecteWord);
+
+const correctLetters = [...selecteWord];
+console.log(correctLetters)
+const wrongLetters = [];
+
+function displayWords() {
+
+    word.innerHTML = `${selecteWord.split('').map(letter => `<div class='letter'>${correctLetters.includes(letter) ? letter : ''}</div>`).join()}`
+
+ 
+}
+
+
+displayWords();
